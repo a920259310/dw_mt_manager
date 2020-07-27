f5fc3331489fc4d32f7fe2e12b40fcaa34445914
920259310@qq.com <920259310@qq.com>
2020-07-27 14:02:07 +0800

    添加待实现接口类容

--- src/main/java/top/putop/mi/service/ITaskManagerService.java
+++ src/main/java/top/putop/mi/service/ITaskManagerService.java
@@ -9,42 +9,42 @@ import top.putop.mi.db.model.TaskManager;
 public interface ITaskManagerService extends IBaseService<TaskManager> {
 
     /**
-     * 生成数据处理脚本内类
+     * 生成数据处理脚本类容
      * @param tbId
      * @return
      */
     public Boolean genRunCode(int tbId);
 
     /**
-     * 生成建表脚本内类
+     * 生成建表脚本类容
      * @param tbId
      * @return
      */
     public Boolean genCreateCode(int tbId);
 
     /**
-     * 生成Mysql数据同步脚本内类
+     * 生成Mysql数据同步脚本类容
      * @param tbId
      * @return
      */
     public Boolean genMysqlImportCode(int tbId);
 
     /**
-     * 生成Oracle数据同步脚本内类
+     * 生成Oracle数据同步脚本类容
      * @param tbId
      * @return
      */
     public Boolean genOracleImportCode(int tbId);
 
     /**
-     * 生成Es数据导出脚本内类
+     * 生成Es数据导出脚本类容
      * @param tbId
      * @return
      */
     public Boolean genEsExportCode(int tbId);
 
     /**
-     * 生成Mysql数据导出脚本内类
+     * 生成Mysql数据导出脚本类容
      * @param tbId
      * @return
      */
